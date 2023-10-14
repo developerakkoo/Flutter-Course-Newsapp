31306ad6736aa6071d725931fb0f1f4e9b52943d
akkoo <developerakshayjadhav@gmail.com>
2023-10-14 16:55:06 +0530

    first

---
+++ lib/widgets/articles_widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:newsapp/services/utils.dart';
+import 'package:newsapp/widgets/vertical_spacing.dart';
+
+import '../consts/vars.dart';
+
+class ArticlesWidget extends StatelessWidget {
+  const ArticlesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = Utils(context).getScreenSize;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).cardColor,
+        child: GestureDetector(
+          onTap: () {},
+          child: Stack(
+            children: [
+              Container(
+                height: 60,
+                width: 60,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              Container(
+                color: Theme.of(context).cardColor,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: FancyShimmerImage(
+                        height: size.height * 0.12,
+                        width: size.height * 0.12,
+                        boxFit: BoxFit.fill,
+                        imageUrl:
+                            "https://techcrunch.com/wp-content/uploads/2022/01/locket-app.jpg?w=1390&crop=1",
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'title ' * 100,
+                            textAlign: TextAlign.justify,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: smallTextStyle,
+                          ),
+                          const VerticalSpacing(5),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              '🕒 Reading time',
+                              style: smallTextStyle,
+                            ),
+                          ),
+                          FittedBox(
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.link,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                Text(
+                                  '20-2-2020 ' * 2,
+                                  maxLines: 1,
+                                  style: smallTextStyle,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
